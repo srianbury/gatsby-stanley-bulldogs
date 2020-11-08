18dad19896a02bebe74eaa2018ef72ed1b5da6f2
Brian Sunbury <bsunbury29@gmail.com>
2020-11-08 15:46:57 -0700

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,13 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN_PROD,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        defer: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
