f471da4ae7e9e8966408d1760a17e49d1950d299
Brian Sunbury <bsunbury29@gmail.com>
2020-11-09 20:45:53 -0700

    mom and dad refs for litters

--- src/pages/litters.js
+++ src/pages/litters.js
@@ -9,10 +9,31 @@ const Litters = ({ data }) => (
     <Container>
       <h1>Our Litters</h1>
       <div className="pl-3 pr-3">
-        {data.allContentfulLitter.edges.map(({ node }) => (
+        {data.allContentfulLitter.edges.map(({ node }, index) => (
           <div key={node.id} className="mb-4">
-            <div className="row">Mom: {node.mom}</div>
-            <div className="row">Dad: {node.dad}</div>
+            <div className="row">
+              <h5>Litter {index + 1}</h5>
+            </div>
+            <div className="row">
+              <div className="col-6 col-md-4 pl-0 pr-0">
+                <div>
+                  <GtsbImg alt="" fluid={node.momRef.image.fluid} />
+                </div>
+                <div>
+                  <h4 className="mb-0 d-inline">{node.momRef.name}</h4> (Mom)
+                </div>
+                <div>Breed: {node.momRef.breed}</div>
+              </div>
+              <div className="col-6 col-md-4 pl-0 pr-0">
+                <div>
+                  <GtsbImg alt="" fluid={node.dadRef.image.fluid} />
+                </div>
+                <div>
+                  <h4 className="mb-0 d-inline">{node.dadRef.name}</h4> (Dad)
+                </div>
+                <div>Breed: {node.dadRef.breed}</div>
+              </div>
+            </div>
             <div className="row">Birthday: {node.birthday}</div>
             <div className="row">
               {node.images.map(image => (
@@ -38,8 +59,24 @@ export const query = graphql`
       edges {
         node {
           id
-          mom
-          dad
+          momRef {
+            name
+            breed
+            image {
+              fluid(maxWidth: 350, resizingBehavior: SCALE) {
+                ...GatsbyContentfulFluid_tracedSVG
+              }
+            }
+          }
+          dadRef {
+            name
+            breed
+            image {
+              fluid(maxWidth: 350, resizingBehavior: SCALE) {
+                ...GatsbyContentfulFluid_tracedSVG
+              }
+            }
+          }
           birthday(formatString: "MMMM, YYYY")
           images {
             id
